2a203657bdf44df61dcd3e34ceb3e3c583508965
ebikasplace <unofficial.xp1@gmail.com>
2025-12-20 08:08:26 +0000

    Fourteenth Commit "Lawd have mercy"

--- models/Order.js
+++ models/Order.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema({
   userId: { type: String, required: true }, 
   items: [{
-      productId: { type: String, required: true, ref: 'product' },
+      product: { type: String, required: true, ref: 'product' },
       quantity: { type: Number, required: true },
   }],
   amount: { type: Number, required: true },
